21f832b0d9fe74a15eb41f0c1b4a93d8463cc317
Ross <rosskeene196@gmail.com>
2023-01-08 22:58:34 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,34 +1,64 @@
-import { useEffect , useState} from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-const [quoteInfo, setQuoteInfo] = useState({});
+  const [quoteInfo, setQuoteInfo] = useState({});
+  const [bgColor, setBgColor] = useState("");
 
-useEffect(()=> {
-  getQuote ()
-},[])
+  const colors = [
+    "#FF6633",
+    "#FFB399",
+    "#FF33FF",
+    "#FFFF99",
+    "#00B3E6",
+    "#E6B333",
+    "#3366E6",
+  ];
 
-const getQuote = () => {
-  fetch('https://api.quotable.io/random')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    setQuoteInfo({
-      text: data.content,
-      author: data.author,
-    });
-  });
-};
+  const setRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * 7);
+    const randomColor = colors[randomIndex];
+    setBgColor(randomColor);
+  };
 
+  useEffect(() => {
+    getQuote();
+    
+  }, []);
+
+  const getQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setQuoteInfo({
+          text: data.content,
+          author: data.author,
+        });
+        setRandomColor();
+      });
+  };
+console.log(bgColor)
   return (
-    <div className="App">
-   <div id="quote-box">
-    <p id="text">{quoteInfo.text}</p>
-    <p id="author">{quoteInfo.author}</p>
-    <button onClick={getQuote} id="new-quote">New Quote</button>
-    <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text' + quoteInfo.text} target="_top" id="tweet-quote" >Post to Twitter</a>
-   </div>
+    <div className="App" style={{backgroundColor: bgColor}}>
+      <div id="quote-box">
+        <p id="text">{quoteInfo.text}</p>
+        <p id="author">{quoteInfo.author}</p>
+        <button onClick={getQuote} id="new-quote">
+          New Quote
+        </button>
+        <a
+          href={
+            "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text" +
+            quoteInfo.text
+          }
+          target="_top"
+          id="tweet-quote"
+        >
+          Post to Twitter
+        </a>
+      </div>
     </div>
   );
 }
